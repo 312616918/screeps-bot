4269300951ba25caaa01589e00ed3dc73c566e3c
tiliang <312616918@qq.com>
2021-07-20 14:46:10 +0800

    modified:   global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -3,26 +3,19 @@
 // import { Memory, CreepMemory } from "./node_modules/@types/screeps/index.d.ts"
 export {}
 
-interface WorkPos {
-    x: number,
-    y: number,
-    roomName: string
-}
-
 
 declare global {
 
     type reqType = "output" | "input" | "pickup";
     type RoomName = "W3N15" | "W3N19"
-        | "W2N15" | "W2N16" | "W2N18"
-        | "W7N16";
+        | "W2N15" | "W2N16" | "W7N16";
 
     interface Memory {
         facility: {
             [roomName: string]: {
                 sources: {
                     [sourceId: string]: {
-                        harvestPos: WorkPos,
+                        harvestPos: RoomPosition,
                         containerId?: string,
                         towerIds?: string[],
                         linkId?: string,
@@ -32,17 +25,14 @@ declare global {
                 mineral?: {
                     id: string,
                     resourceType: MineralConstant,
-                    harvestPos: WorkPos,
+                    harvestPos: RoomPosition,
                     containerId?: string
                 }
                 upgrade?: {
-                    workPos: WorkPos,
+                    RoomPosition: RoomPosition,
                     containerId?: string,
                     linkId?: string,
                     towerIds?: string[]
-                },
-                dispatch?: {
-                    workPos: WorkPos
                 },
                 terminalId?: string,
                 storageId?: string,
@@ -57,39 +47,19 @@ declare global {
                 }
             }
         }
-        carryReqs: {
-            [roomName: string]: {
-                [reqId: string]: {
-                    id: string;
-                    type: reqType;
-                    resourceType: ResourceConstant;
-                    amount: number;
-                    reserve: number;
+        carry: {
+            task:{
+                [roomName: string]: {
+                    [reqId: string]: {
+                        id: string;
+                        type: reqType;
+                        resourceType: ResourceConstant;
+                        amount: number;
+                        reserve: number;
+                    }
                 }
             }
         }
-        dispatchReqs: {
-            [roomName: string]: {
-                [reqId: string]: RoomDispatchReq
-            }
-        }
-        centerSource: {
-            [sourceId: string]: CenterSource
-        }
-        roomConfig: {
-            [roomName in RoomName]?: {
-                curReact: ResourceConstant;
-
-            }
-        }
-    }
-
-    interface RoomDispatchReq {
-        id: string;
-        type: reqType;
-        resourceType: ResourceConstant;
-        amount: number;
-        priority: number;
     }
 
     interface SpawnTemplate {
@@ -101,44 +71,19 @@ declare global {
     }
 
     interface CreepMemory {
-        targetId?: string;
-        sourceId?: string;
-        needEnergy?: number;
-        workRoom?: string;
-        roomName?: string;
-        reqId?: string;
-        reserve?: number;
-        resourceType?: ResourceConstant;
-        workPos?: WorkPos;
-    }
-
-    interface CenterSource {
-        enable: boolean;
-        isSafe: boolean;
-        isInvaded: boolean;
-        safePoint: WorkPos;
-        harvestPos: WorkPos;
-        targetId: string;
-        containerPos?: WorkPos;
-    }
-
-    type RoomResAmount = {
-        [roomName in RoomName]?: {
-            terminal: {
-                [rType in ResourceConstant]?: {
-                    tMaxAmount: number;
-                    tMinAmount: number;
-
-                    sMinAmount?: number;
-                }
-            },
-            /**
-             * 房间最低资源量，低于此值，尝试从其他房间调配
-             */
-            minAmount?: {
-                [rType in ResourceConstant]?: number;
-            }
-
+        carry:{
+            tasks:{
+            
+            }[];
+        }
+        harvest:{
+            targetId:string;
+            workPos:RoomPosition;//属性缺失表示就位
+        }
+        bulid:{
+            targetId:string;
+            energyReq:number;
+            energyRes:number;
         }
     }
 
